e2ae69f8f159def0c1c01d3cb4b8f0dbc944c11a
Caio Lima <caioifg@gmail.com>
2023-03-17 17:08:13 -0300

    ajuste do projeto

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,} from 'react-native';
+
+
+
+
+export default function App() {
+
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+ 
+});
+
